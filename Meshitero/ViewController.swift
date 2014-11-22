5b295c876439f05d77493c3ed6395db88486790e
naoyashiga <naoyashiga0@gmail.com>
2014-11-22 20:22:23 +0900

    【add】初回コミット

--- Meshitero/ViewController.swift
+++ Meshitero/ViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource{
+    var collectionView: UICollectionView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //レイアウト作成
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right :10)
+        layout.itemSize = CGSize(width: 90, height: 120)
+        
+        //コレクションビューのインスタンスを作成
+        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionView!.dataSource = self
+        collectionView!.delegate = self
+        //カスタムセルではないのでUICollectionViewCellをセット
+        collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView!.backgroundColor = UIColor.whiteColor()
+        self.view.addSubview(collectionView!)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
